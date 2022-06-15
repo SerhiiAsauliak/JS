1448d1f0106ef5f33a5c49a1e9f5910b505c714f
Serhii <sergiomakafon@gmail.com>
2022-06-15 17:20:26 +0300

    added simple sliders creator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-slider1.start();
+
+let slider1 = sliderFactory.createSlider();
+let slider2 = sliderFactory.createSlider();
+let slider3 = sliderFactory.createSlider();
+
+slider1.start('slider-one');
+slider2.start('slider-two');
+slider3.start('slider-three');
